f74c27df5944e3425de7393980d5b9a3e2a7de15
Tuuz <silverhawk@foxmail.com>
2021-02-03 22:02:45 +0800

    TuuzPC

--- lib/config/url.dart
+++ lib/config/url.dart
@@ -1,3 +1,5 @@
 class Url {
-  String User_info = "/v1/user/user/user_info";
+  String User_info = "/v1/user/user/info";
+  String User_balance = "/v1/user/user/balance";
+  String User_balance_record = "/v1/user/user/balance_record";
 }
