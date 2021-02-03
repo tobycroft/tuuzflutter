7d87114a27cb51e7aaad3a85ad9cba016fe6e4ee
Tuuz <silverhawk@foxmail.com>
2021-02-03 19:34:46 +0800

    TuuzPC

--- lib/config/auth.dart
+++ lib/config/auth.dart
@@ -1,3 +1,16 @@
+import 'package:TuuzFlutter/app/login/login.dart';
+import 'package:TuuzFlutter/tuuz/win/close.dart';
+import 'package:flutter/cupertino.dart';
+
 class Auth {
   bool Is_login = false;
+
+  bool login(BuildContext context, Map json) {
+    if (json["code"] == -1) {
+      Windows().Open(context, Login());
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
