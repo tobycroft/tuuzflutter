a66e5b58dd235a73156db7839f3ee4a71f32faaf
Tuuz <silverhawk@foxmail.com>
2021-02-03 23:17:16 +0800

    TuuzPC

--- lib/config/auth.dart
+++ lib/config/auth.dart
@@ -1,9 +1,21 @@
 import 'package:TuuzFlutter/app/login/login.dart';
+import 'package:TuuzFlutter/tuuz/storage/storage.dart';
 import 'package:TuuzFlutter/tuuz/win/close.dart';
 import 'package:flutter/cupertino.dart';
 
 class Auth {
-  bool Is_login ;
+  bool Is_login;
+
+  Future<bool> Check_login() async {
+    bool uid = await Storage().Has("__uid__");
+    bool token = await Storage().Has("__token__");
+
+    if (uid && token) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   bool Return_login_check_and_Goto(BuildContext context, Map json) {
     if (json["code"] == -1) {
