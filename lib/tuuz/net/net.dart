168ac2713baf08fcefc91fd6907d680976a4c53a
Tuuz <silverhawk@foxmail.com>
2021-02-04 20:22:47 +0800

    TuuzPC

--- lib/tuuz/net/net.dart
+++ lib/tuuz/net/net.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:TuuzFlutter/config/config.dart';
+import 'package:gobotq_flutter/config/config.dart';
 
 class Net {
   Future<String> Post(String url, path, Map<String, String> get, Map<String, dynamic> post, Map<String, String> header) async {
